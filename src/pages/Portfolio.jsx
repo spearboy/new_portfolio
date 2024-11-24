d3ef1ca27fc03db8ca1143dba83beb02c23420c5
spearboy <95023009+spearboy@users.noreply.github.com>
2024-11-25 03:45:10 +0900

    fix scss

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,6 +14,7 @@ import whatmovie from '../assets/img/whatmovie.png';
 import kboland from '../assets/img/kboland.png';
 import musicflix from '../assets/img/musicflix.png';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { FaChevronRight } from 'react-icons/fa';
 
 const Portfolio = () => {
     return (
@@ -48,8 +49,8 @@ const Portfolio = () => {
                                 mySQL을 이용해 회원정보를 관리하며,
                                 나만의 반려견 커스텀 옷을 자랑 할 수 있는 커뮤니티 제작</p>
                             <div className='urltexts'>
-                                <a href="https://github.com/spearboy/project_pickstom" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="http://spearboy.dothome.co.kr/" target='_blank' rel="noreferrer">Site view</a>
+                                <a href="https://github.com/spearboy/project_pickstom" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="http://spearboy.dothome.co.kr/" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
@@ -67,8 +68,8 @@ const Portfolio = () => {
                             <p className='desc'>React.js 를 이용한 신용카드 모아보기 사이트.<br />
                                 Python을 이용해 네이버 신용카드 페이지를 스크랩하여 데이터를 추출하였습니다. 해당 데이터를 보기 좋게 React.js으로 만들었습니다.</p>
                             <div className='urltexts'>
-                                <a href="https://github.com/spearboy/carddive" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="https://cardive.vercel.app/" target='_blank' rel="noreferrer">Site view</a>
+                                <a href="https://github.com/spearboy/carddive" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="https://cardive.vercel.app/" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
@@ -86,8 +87,8 @@ const Portfolio = () => {
                             <p className='desc'>호요버스의 간판 타이틀 원신의 PC라운지 페이지.<br />
                                 제작사의 요청으로 외주작업을 진행 하였습니다. 기존 작업사의 ftp 정보를 받아 자택에서 작업하였습니다.</p>
                             <div className='urltexts'>
-                                <a className='disabled-link' onclick="return false;" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="http://gipclounge.com/" target='_blank' rel="noreferrer">Site view</a>
+                                <a className='disabled-link' onclick="return false;" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="http://gipclounge.com/" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
@@ -105,8 +106,8 @@ const Portfolio = () => {
                             <p className='desc'>Vue.js와 TMDB API를 이용하여 제작한 사이트.<br />
                                 TMDB API를 이용하여 세계박스오피스, 출연진, 예고편영상 등의 데이터를 가져와 보기 좋게 Vue.js를 사용해 구현 하였습니다.</p>
                             <div className='urltexts'>
-                                <a href="https://github.com/spearboy/what_movie" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="https://moviecbh.vercel.app/" target='_blank' rel="noreferrer">Site view</a>
+                                <a href="https://github.com/spearboy/what_movie" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="https://moviecbh.vercel.app/" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
@@ -124,8 +125,8 @@ const Portfolio = () => {
                             <p className='desc'>React.js와 YOUTUBE API를 이용하여 제작한 사이트.<br />
                                 YOUTUBE API를 이용하여 한국 프로야구 팀에 대한 영상과 간단한 팀 정보를 제공하는 웹사이트입니다.</p>
                             <div className='urltexts'>
-                                <a href="https://github.com/spearboy/kboland" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="https://kboland.vercel.app/" target='_blank' rel="noreferrer">Site view</a>
+                                <a href="https://github.com/spearboy/kboland" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="https://kboland.vercel.app/" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
@@ -143,8 +144,8 @@ const Portfolio = () => {
                             <p className='desc'>React.js와 YOUTUBE API를 이용하여 제작한 사이트.<br />
                                 Python을 이용하여 멜론,벅스,지니 등 각 음악차트 사이트의 순위를 매일 수집하는 Action을 깃허브에 적용해 데이터를 가져옵니다. 이후 YOUTUBE API를 이용해 여러 음악을 스트리밍 하고 플레이리스트에 저장할 수 있게 만든 사이트 입니다.</p>
                             <div className='urltexts'>
-                                <a href="https://github.com/spearboy/yt_music" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="https://musicflixcbh.vercel.app/" target='_blank' rel="noreferrer">Site view</a>
+                                <a href="https://github.com/spearboy/yt_music" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="https://musicflixcbh.vercel.app/" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
@@ -163,8 +164,8 @@ const Portfolio = () => {
                                 Next.js를 이용해 프론트를 구성했으며, Node.js Express 를 이용해 홈서버를 구축해 개발중입니다.
                                 여러명이서 같이 주문하는 그런 시스템임으로 웹소켓이 적용되어있습니다. 메뉴를 담고 일행과 나의 메뉴를 장바구니에서 확인할 수 있습니다.</p>
                             <div className='urltexts'>
-                                <a href="https://github.com/spearboy/project_qbit" target='_blank' rel="noreferrer">Code view</a>
-                                <a href="http://qbitorder.com" target='_blank' rel="noreferrer">Site view</a>
+                                <a href="https://github.com/spearboy/project_qbit" target='_blank' rel="noreferrer">Code view <FaChevronRight /></a>
+                                <a href="http://qbitorder.com" target='_blank' rel="noreferrer">Site view <FaChevronRight /></a>
                             </div>
                         </div>
 
